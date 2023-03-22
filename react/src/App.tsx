2225da48c0a91db0cb6b2d6b3f86805f16b9e1dc
NxN-2453 <nahommulugeta39@gmail.com>
2023-03-22 21:18:53 +0300

    Props and states

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -10,9 +10,24 @@ import ListGroup from "./components/ListGroup";
 // }
 
 function App() {
+  let items = [
+    "An item",
+    "A second item",
+    "A third item",
+    "A fourth item",
+    "And a fifth one",
+  ];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
   return (
     <div>
-      <ListGroup />
+      <ListGroup
+        items={items}
+        heading="Items"
+        onSelectItem={handleSelectItem}
+      />
     </div>
   );
 }
--- react/src/components/ListGroup.tsx
+++ react/src/components/ListGroup.tsx
@@ -1,24 +1,34 @@
-function ListGroup() {
-  let items = [
-    "An item",
-    "A second item",
-    "A third item",
-    "A fourth item",
-    "And a fifth one",
-  ];
+import { useState } from "react";
+interface Props {
+  items: string[];
+  heading: string;
+  onSelectItem: (item: string) => void;
+}
+function ListGroup({ items, heading, onSelectItem }: Props) {
+  // State Hook
+  const [selectedItem, setSelectedItem] = useState(-1);
+
   //   items = [];
 
   items.map((item) => <li>{item}</li>);
+
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
       {items.length === 0 && <p>No items</p>}
       <ul className="list-group">
-        {items.map((item) => (
+        {items.map((item, index) => (
           <li
-            className="list-group-item"
+            className={
+              selectedItem === index
+                ? "list-group-item active"
+                : "list-geoup-item"
+            }
             key={item}
-            onClick={() => console.log("Clicked", item)}
+            onClick={() => {
+              setSelectedItem(index);
+              onSelectItem(item);
+            }}
           >
             {item}
           </li>
