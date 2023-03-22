5601baa212f32001e26febf10fb9f3c6c5299c16
NxN-2453 <nahommulugeta39@gmail.com>
2023-03-22 21:41:32 +0300

    Alert

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,33 +1,10 @@
-import Message from "./Message";
-import ListGroup from "./components/ListGroup";
-
-// function App(){
-//     const name = "John";
-//     if (name === "John") {
-//         return <h1>Hello {name}</h1>;
-//     }
-//     return <h1>Hello World</h1>;
-// }
-
+import Alert from "./components/Alert";
 function App() {
-  let items = [
-    "An item",
-    "A second item",
-    "A third item",
-    "A fourth item",
-    "And a fifth one",
-  ];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Items"
-        onSelectItem={handleSelectItem}
-      />
+      <Alert>
+        Hello <span>World</span>
+      </Alert>
     </div>
   );
 }
